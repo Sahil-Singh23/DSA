7ffb0c40c19a659d51032816f50d037e1d8c4b60
Sahil Singh <141420004+Sahil-Singh23@users.noreply.github.com>
2026-07-08 20:10:53 +0530

    Time: 13 ms (76.1%), Space: 47.7 MB (95.53%) - LeetHub

--- 3983-subsequence-after-one-replacement/3983-subsequence-after-one-replacement.java
+++ 3983-subsequence-after-one-replacement/3983-subsequence-after-one-replacement.java
@@ -1,11 +1,15 @@
 class Solution {
     public boolean canMakeSubsequence(String s, String t) {
-        int i = 0, j = 0, m = s.length();
-        for (char c : t.toCharArray()) {
-            if (i < m && s.charAt(i) == c) i++;
-            i = Math.max(i, j + 1);
-            if (j < m && s.charAt(j) == c) j++;
-            if (i == m || j == m) return true;
+        
+        int n = s.length();
+        int a=0;
+        int b=0;
+
+        for(int i=0 ; i<t.length() ; i++){
+            if(s.charAt(a)==t.charAt(i)) a++;
+            a = Math.max(a,b+1);
+            if(s.charAt(b)==t.charAt(i)) b++;
+            if(a==n || b == n ) return true;
         }
         return false;
     }
