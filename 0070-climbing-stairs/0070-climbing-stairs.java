54079f94814fc2a021632f42d8dceab8eea855e6
Sahil Singh <141420004+Sahil-Singh23@users.noreply.github.com>
2026-08-15 13:15:59 +0530

    Time: 0 ms (100%), Space: 42.1 MB (36.59%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,21 @@
+class Solution {
+    
+    public int climbStairs(int n) {
+        int dp[] = new int[n+1];
+        Arrays.fill(dp,-1);
+        return solve(0,n,dp);
+    }
+
+    int solve(int i, int n, int dp[]){
+        if(i>=n){
+            if(i==n){
+                return 1;
+            }
+            return 0;
+        }
+        if(dp[i]!=-1) return dp[i];
+
+       return dp[i] = solve(i+1,n,dp)+
+        solve(i+2,n,dp);
+    }
+}
